9383bdbf000b49281643d51ace8934af59a7f77e
foysalthecode <ahmedmdfoysal123@gmail.com>
2026-02-17 23:32:37 +0600

    added navbar

---
+++ src/app/(commonLayout)/layout.tsx
@@ -0,0 +1,14 @@
+import { Navbar } from "@/components/layout/navbar1";
+
+export default function CommonLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      <Navbar></Navbar>
+      {children}
+    </div>
+  );
+}
